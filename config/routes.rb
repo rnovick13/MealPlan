af3b207580ca49058636599c052b2e8f1448865d
rnovick13 <rnovick13@gmail.com>
2020-07-03 10:02:35 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get "homes/index"
+    root 'homes#index'
+    resources :meal_plans, only: [:index, :show, :new, :create]
+    resources :meal_restrictions, only: [:index, :show, :new, :create]
+    resources :meals, only: [:index, :show, :new, :create, :edit, :update]
+    resources :mp_meals, only: [:index, :new, :create]
+    resources :restrictions, only: [:index, :show, :new, :create]
 end
